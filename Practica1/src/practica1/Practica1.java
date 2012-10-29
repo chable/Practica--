6d4ad88c323f588bc840396804076145c23b03c2
chable <robert_dpinio@hotmail.com>
2012-10-29 16:22:13 -0600

    agregando proyecto en java

---
+++ Practica1/src/practica1/Practica1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practica1;
+
+/**
+ *
+ * @author Jorge Dominguez J
+ */
+public class Practica1
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        // TODO code application logic here
+    }
+}
