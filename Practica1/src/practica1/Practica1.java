08261de7352e0e65f1cf3ad01fb4b86588319975
chable <robert_dpinio@hotmail.com>
2012-11-06 14:43:54 -0600

    caso del 1 al 5

--- Practica1/src/practica1/Practica1.java
+++ Practica1/src/practica1/Practica1.java
@@ -4,6 +4,7 @@
  */
 package practica1;
 
+import com.sun.xml.internal.ws.api.pipe.NextAction;
 import java.util.Scanner;
 
 /**
@@ -20,9 +21,9 @@ public class Practica1
      
             
     {
-         System.out.print("INSTITUTO TECNOLOGICO SUPERIOR DE ESCARCEGA\n\n ROBERT ALEXANDER RAMIREZ CHABLE\n\n ELIGIR OPCION: 1,2,3,4,5");
+         System.out.print("INSTITUTO TECNOLOGICO SUPERIOR DE ESCARCEGA\n\n NOMBRE DEL ALUMNO: ROBERT ALEXANDER RAMIREZ CHABLE\n\n ELIGIR OPCION:\n\n 1 ayuda sobre el funcionamiento\n\n 2 cadena invertida\n\n 3 largo de la cadena\n\n 4 inicio y fin de la subcadena\n\n 5 sacar un intervalo de la cadena");
        
-         
+       
         int opcion=0;
         Scanner lectura = new Scanner(System.in);
         
@@ -53,9 +54,32 @@ public class Practica1
                     System.out.println("El largo de la Cadena es : " + palabra.length());
                  break;
                       
+                 case 4:
+                     System.out.println("Ingresar el texto ");
+                     String nombre = lectura.next();
+                     System.out.println("La subcadena entre la posición 3 y 7 es : " + nombre.substring(3,8));
+                     
                  
 
-                      
+                break;
+                     
+                case 5:
+                 
+                   int Inicial=0;
+                   int Final=0;
+                     System.out.println("Ingresar el texto ");
+                     String ROBERT=lectura.next();
+                    
+
+                     System.out.println("Escribe un numero final mayor, menor o igual que:  " + (ROBERT.length( )));
+                     Final = lectura.nextInt();              
+                     System.out.println("Escribe un numero inicial mayor o Igual a cero pero que sea menor que el numero final ");
+                     Inicial = lectura.nextInt();
+                     int res = Final-Inicial;
+                     System.out.print("El numero de rango de la palabra: " + ROBERT + " es= " );
+                     System.out.print(res);
+                 break; 
+                         
        
     }
     }
---
+++ Practica1/src/practica1/letras.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practica1;
+
+/**
+ *
+ * @author Jorge Dominguez J
+ */
+class letras
+{
+    
+}
